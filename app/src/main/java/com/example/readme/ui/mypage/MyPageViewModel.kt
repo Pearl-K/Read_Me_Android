ca0550559a71be790d330458ea3bbfe2bc81f43b
Pearl <rkdwlswn227@gmail.com>
2024-08-05 21:50:13 +0900

    edit: MyPageViewModel 추가수정

--- app/src/main/java/com/example/readme/ui/mypage/MyPageViewModel.kt
+++ app/src/main/java/com/example/readme/ui/mypage/MyPageViewModel.kt
@@ -3,8 +3,12 @@ package com.example.readme.ui.mypage
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 
 class MyPageViewModel : ViewModel() {
+    //private val profileRepository = ProfileRepository()
+
     private val _profileName = MutableLiveData("Username")
     val profileName: LiveData<String> get() = _profileName
 
@@ -20,4 +24,22 @@ class MyPageViewModel : ViewModel() {
     private val _followingCount = MutableLiveData(777)
     val followingCount: LiveData<Int> get() = _followingCount
 
+    private val _profileImg = MutableLiveData<String>()
+    val profileImg: LiveData<String> get() = _profileImg
+
+    fun fetchProfile() {
+        viewModelScope.launch {
+            //val response = profileRepository.getProfile()
+            /*if (response.isSuccess) {
+                response.result?.let { profile ->
+                    _profileName.value = profile.nickname
+                    _profileId.value = "@${profile.account}"
+                    _profileBio.value = profile.comment
+                    _followersCount.value = profile.followerNum
+                    _followingCount.value = profile.followingNum
+                    _profileImg.value = profile.profileImg
+                }
+            }*/
+        }
+    }
 }
